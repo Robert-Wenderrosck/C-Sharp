56a027ee14b1f841008ebd705e19ddcb59ea943e
Robert Wenderrosck <wenderrosckrc@gmail.com>
2025-07-14 22:02:34 -0300

    Add files via upload

---
+++ While/Soma com While.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int soma = 0;
+
+            while (contador <= 10) 
+            {
+                soma += contador;
+                Console.WriteLine("Adicionado " + contador + " à soma atual: " + soma);
+
+                contador++;
+             }
+
+            Console.ReadKey();
+        }
+    }
+}
