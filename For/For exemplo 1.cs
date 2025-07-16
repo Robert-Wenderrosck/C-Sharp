908e4dfe9e7bc625a88e6a529f34f97c55c02e8b
Robert Wenderrosck <wenderrosckrc@gmail.com>
2025-07-15 21:37:17 -0300

    Add files via upload

---
+++ For/For exemplo 1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_exemplo_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int posicao = 0 (Inicio do For)
+            //posicao < 10 (Executar até)
+            //posicao++ (Incremento)
+            for (int posicao = 0; posicao < 10; posicao++)
+            {
+                Console.WriteLine(posicao);
+            }
+
+
+        }
+    }
+}
---
+++ For/For exemplo 2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_exemplo_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i<= 100; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    //Imprime a mensagem indicando que o número é par
+                    Console.WriteLine(i + " é um número par");
+                }
+            }
+            
+            Console.WriteLine("-----------------------------");
+
+            //Cria um array de inteiros
+            int[] numeros = { 10, 25, 30, 45, 50 };
+            int soma = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+
+                soma += numeros[i];
+            }
+
+            double media = (double)soma / numeros.Length;
+            Console.WriteLine("A média dos números é: " + media);
+
+        }
+    }
+}
