2e27ab93946dfd64dc7a994b3e9bd76d213dc522
Robert Wenderrosck <wenderrosckrc@gmail.com>
2025-07-20 16:48:31 -0300

    Add files via upload

---
+++ Array e Matriz/Array exemplo 1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declaração e inicialização de um array de inteiros
+            int[] numeros = { 10, 20, 25, 45, 19 };
+
+            //Exibir os valores do array ma tela 
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+
+            }
+
+            Console.WriteLine("\n\n----------------Array de Strings ----------------");
+
+            string[] cores = new string[4]; //Cria um novo array com 4 posições
+
+            cores[0] = "vermelho";
+            cores[1] = "verde";
+            cores[2] = "azul";
+            cores[3] = "amarelo";
+
+            foreach (string cor in cores)
+            {
+                Console.WriteLine(cor);
+            }
+
+
+        }
+    }
+}
