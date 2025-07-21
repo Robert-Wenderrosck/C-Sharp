19c53d4f98dbdab88a3c9f094bf490a3c706bec9
Robert Wenderrosck <wenderrosckrc@gmail.com>
2025-07-21 14:23:44 -0300

    Add files via upload

---
+++ POO - Programação Orientada a Objeto/Conta Bancaria.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta_Bancaria
+{
+
+    class ContaBancaria
+    {
+        //Atributos
+        private string titular;
+        private int numero;
+        private double saldo;
+
+        //Construtor da classe que recebe o nome, numero e saldo
+        public ContaBancaria(string titularConta, int numeroConta)
+        {
+            //Atribui os valores dos parâmetros aos atributos da classe
+            this.titular = titularConta;
+            this.numero = numeroConta;
+            this.saldo = 0.0;
+        }
+
+        //Métodos
+        //Método público que retorna o nome do titular da conta
+        public string GetTitular()
+        {
+            return titular;
+        }
+        //Método público que retorna o número da conta
+        public int GetNumero()
+        {
+            return numero;
+        }
+        //Método público que retorna o saldo da conta
+        public double GetSaldo()
+        {
+            return saldo;
+        }
+
+        //Método público que realiza um depósito na conta
+        public void Depositar(double valor)
+        {
+            saldo += valor; //saldo = saldo + valor
+        }
+        //Método público para sacar um valor da conta
+        public void Sacar(double valor)
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor; //saldo = saldo - valor
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente");
+            }
+
+        }
+        public override string ToString()
+        {
+            return "Titular: " + titular +
+                    "\nNúmero da conta: " + numero +
+                    "\nSaldo: " + saldo + "\n";
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chamou a classe e passou as informações 
+            ContaBancaria minhaconta = new ContaBancaria("João da Silva", 12345);
+
+            Console.WriteLine("=========== Dados do Usuário =============\n");
+            Console.WriteLine(minhaconta.ToString());
+
+            minhaconta.Depositar(500);
+
+            Console.WriteLine("=========== Depósito =============\n");
+            Console.WriteLine("Titular: " + minhaconta.GetTitular());
+            Console.WriteLine("Número da Conta: " + minhaconta.GetNumero());
+            Console.WriteLine("Valor Depositado: 500,00\n");
+
+            Console.WriteLine("=========== Saldo após Depósito ===============");
+            Console.WriteLine(minhaconta.ToString());
+
+            minhaconta.Sacar(200);
+
+            Console.WriteLine("=========== Saque =============\n");
+            Console.WriteLine("Titular: " + minhaconta.GetTitular());
+            Console.WriteLine("Número da Conta: " + minhaconta.GetNumero());
+            Console.WriteLine("Valor Saque: 200,00\n");
+
+            Console.WriteLine("=========== Saldo após Saque ===============");
+            Console.WriteLine(minhaconta.ToString());
+
+
+        }
+    }
+}
---
+++ POO - Programação Orientada a Objeto/Lista de Contato.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_Contato
+{
+    class Contato
+    {
+        //Atributos 
+        private string nome;
+        private string email;
+        private string telefone;
+
+        //Construtor 
+        public Contato(string nomeContato, string emailContato, string telefoneContato)
+        {
+            //Atribui os valores dos parâmetros aos campos correspondentes da classe
+            this.nome = nomeContato;
+            this.email = emailContato;
+            this.telefone = telefoneContato;
+        }
+
+        //Métodos get e set
+        public string Nome
+        {
+            get { return nome; } // Retorna o valor do campo 
+            set { nome = value; } //Define um novo valor para o campo (substitui)
+        }
+        public string Email
+        {
+            get { return email; } // Retorna o valor do campo 
+            set { email = value; } //Define um novo valor para o campo (substitui)
+        }
+        public string Telefone
+        {
+            get { return telefone; } // Retorna o valor do campo 
+            set { telefone = value; } //Define um novo valor para o campo (substitui)
+        }
+
+    }
+    class GerenciadorDeContatos
+    {
+        private List<Contato> contatos = new List<Contato>(); //Cria um campo privado do tipo List para armazenar os contatos
+
+        //Método para adicionar um novo contato
+        public void AdicionarContato()
+        {
+            //Solicita o nome do contato ao usuário e armazena em uma variável local 
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+
+            //Solicita o email do contato ao usuário e armazena em uma variável local 
+            Console.WriteLine("E-mail: ");
+            string email = Console.ReadLine();
+
+            //Solicita o telefone do contato ao usuário e armazena em uma variável local 
+            Console.WriteLine("Telefone: ");
+            string telefone = Console.ReadLine();
+
+            //Cria um novo objeto Contato com as informações fornecidas pelo usuário e adiciona à lista de contatos
+            Contato novoContato = new Contato(nome, email, telefone);
+            contatos.Add(novoContato);
+
+            Console.WriteLine("Contato adicionado com sucesso!");
+        }
+
+        public void EditarContato()
+        {
+            Console.Write("Digite o nome do contato que deseja alterar: ");
+            string nome = Console.ReadLine();
+
+            //Procura o contato na lista de contatos pelo nome 
+            Contato contatoParaAlterar = contatos.Find(contato => contato.Nome == nome);
+
+            if (contatoParaAlterar != null)
+            {
+
+                Console.WriteLine("Nome: " + contatoParaAlterar.Nome);
+                Console.WriteLine("E-mail: " + contatoParaAlterar.Email);
+                Console.WriteLine("Telefone: " + contatoParaAlterar.Telefone);
+
+                //Solicita o nome do contato ao usuário e armazena em uma variável local 
+                Console.WriteLine("Novo Nome: ");
+                contatoParaAlterar.Nome = Console.ReadLine();
+
+                //Solicita o email do contato ao usuário e armazena em uma variável local 
+                Console.WriteLine("Novo E-mail: ");
+                contatoParaAlterar.Email = Console.ReadLine();
+
+                //Solicita o telefone do contato ao usuário e armazena em uma variável local 
+                Console.WriteLine("Novo Telefone: ");
+                contatoParaAlterar.Telefone = Console.ReadLine();
+
+                Console.WriteLine("Contato alterado com sucesso!");
+
+            }
+            else
+            {
+                Console.WriteLine("Contato não encontrado!");
+            }
+
+        }
+
+        public void ExcluirContato()
+        {
+            Console.Write("Digite o nome do contato que deseja excluir: ");
+            string nome = Console.ReadLine();
+
+            Contato contatoParaExcluir = contatos.Find(contato => contato.Nome == nome);
+
+            if (contatoParaExcluir != null)
+            {
+                contatos.Remove(contatoParaExcluir);
+                Console.WriteLine("Contato excluído com sucesso!");
+
+            }
+            else
+            {
+                Console.WriteLine("Contato não encontrado!");
+            }
+        }
+
+        public void ExibirContatos()
+        {
+            if (contatos.Count ==0)
+            {
+                Console.WriteLine("Nenhum contato encontrado.");
+            }
+            else
+            {
+                Console.WriteLine("--------------- Lista de Contatos ---------------- ");
+                //Imprime os contatos salvos na lista
+                foreach (Contato contato in contatos)
+                {
+                    Console.WriteLine("Nome: " + contato.Nome);
+                    Console.WriteLine("E-mail: " + contato.Email);
+                    Console.WriteLine("Telefone: " + contato.Telefone);
+                    Console.WriteLine("---------------------------\n: ");
+                }
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cria uma instância da classe GerenciadorDeContato (ativar) 
+            GerenciadorDeContatos gerenciadorDeContatos = new GerenciadorDeContatos();
+
+            //Laço infinito para mostrar o menu opções 
+            while (true)
+            {
+                //Imprime as opções do Menu
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("1 - Adicionar contato");
+                Console.WriteLine("2 - Editar contato");
+                Console.WriteLine("3 - Excluir contato");
+                Console.WriteLine("4 - Exibir os contatos");
+                Console.WriteLine("0 - Sair");
+
+                //Lê a opção escolhida pelo usuário
+                int opcao = int.Parse(Console.ReadLine());
+
+                switch(opcao)
+                {
+                    case 1:
+                        gerenciadorDeContatos.AdicionarContato();
+                        break;
+                    case 2: 
+                        gerenciadorDeContatos.EditarContato();
+                        break;
+                    case 3:
+                        gerenciadorDeContatos.ExcluirContato();
+                        break;
+                    case 4:
+                        gerenciadorDeContatos.ExibirContatos();
+                        break;
+                    case 0:
+                        Environment.Exit(0); //Encerra o while
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+                //Imprime uma linha em branco para separar a próxima execução do menu
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
